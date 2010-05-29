d94122586799767591f9c42e26ce412fb46dde81
tomsontom <tomsontom@8ff015e9-a363-276f-a91a-a5f1e02c747b>
2010-05-29 13:22:36 +0000

    Initial import

---
+++ bundles/at.bestsolution.translate.services.google/src/at/bestsolution/translate/services/google/GoogleTranslator.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package at.bestsolution.translate.services.google;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+
+import at.bestsolution.translate.services.ITranslator;
+
+import com.google.api.translate.Language;
+import com.google.api.translate.Translate;
+
+public class GoogleTranslator implements ITranslator {
+	private static FromTo[] FROM_TOS;
+	
+	{
+		ArrayList<FromTo> l = new ArrayList<FromTo>();
+		for(Language fromLang : Language.values()) {
+			for( Language toLanguage : Language.values() ) {
+				if( fromLang != toLanguage ) {
+					l.add(new FromTo(fromLang.name(), toLanguage.name()));
+				}
+			}
+		}
+		FROM_TOS =  l.toArray(new FromTo[0]);
+	}
+	
+	public String getName() {
+		return "Google Translate";
+	}
+
+	public FromTo[] getFromTo() {
+		return FROM_TOS;
+	}
+
+	public String translate(FromTo fromTo, String term) throws InvocationTargetException {
+		try {
+			return Translate.execute(term, Language.valueOf(fromTo.from), Language.valueOf(fromTo.to));
+		} catch (Exception e) {
+			throw new InvocationTargetException(e);
+		}
+	}
+}
