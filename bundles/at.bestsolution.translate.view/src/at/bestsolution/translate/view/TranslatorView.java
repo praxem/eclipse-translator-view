721c99bbf16efd3a5cf06f0d10da6dfbdfd2a694
tomsontom <tomsontom@8ff015e9-a363-276f-a91a-a5f1e02c747b>
2010-05-29 15:46:08 +0000

    adding context creation stuff

--- bundles/at.bestsolution.translate.view/src/at/bestsolution/translate/view/TranslatorView.java
+++ bundles/at.bestsolution.translate.view/src/at/bestsolution/translate/view/TranslatorView.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package at.bestsolution.translate.view;
 
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.*;
 
@@ -18,17 +20,30 @@ public class TranslatorView extends ViewPart {
 
 	public static final String ID = "at.bestsolution.translate.view.views.TranslatorView";
 
-	private TranslatorComponent component = new TranslatorComponent();
+	private IEclipseContext context;
+	private TranslatorComponent component;
 
+	public TranslatorView() {
+		
+	}
+	
 	public void createPartControl(Composite parent) {
-		component.createUI(parent);
+		IEclipseContextProvider p = (IEclipseContextProvider) getSite().getService(IEclipseContextProvider.class);
+		context = p.getContext().createChild();
+		context.set(Composite.class, parent);
+		component = ContextInjectionFactory.make(TranslatorComponent.class, context);
 	}
 
-	
 	/**
 	 * Passing the focus request to the viewer's control.
 	 */
 	public void setFocus() {
 		component.setFocus();
+	}
+	
+	@Override
+	public void dispose() {
+		context.dispose();
+		super.dispose();
 	}
 }
